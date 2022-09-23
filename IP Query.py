a658ec648686c88d727d96a89d75dbd54226d186
Pengwei Chen <61215150+Pengwei-Chen@users.noreply.github.com>
2022-09-23 13:21:10 +0800

    Add translation and also create OpenVPN config

--- IP Query.py
+++ IP Query.py
@@ -1,14 +1,47 @@
 import os
 import sys
+import shutil
 import re
 import requests
+from googletrans import Translator
 
 directory = repr(os.path.dirname(os.path.realpath(sys.argv[0]))).strip("'").replace("\\\\", "/") + "/"
 table = open(directory + "Table.txt", "w", encoding = "UTF-8")
-os.chdir(directory + "/Surfshark_Config")
+os.chdir(directory + "Surfshark_Config")
+shutil.rmtree(directory + "OpenVPN Config")
+os.mkdir(directory + "OpenVPN Config")
 for file in os.listdir():
+    if file.find("tcp") != -1:
+        continue
     ip = file.split("_")[0]
     content = requests.get("https://www.ipshudi.com/" + ip + ".htm").text
-    location = re.findall("<td class=\"th\">归属地</td>\n<td>\n<span>(.*?)</span>", content)[0].rstrip(" ")
-    table.write(ip + "\t" + location + "\n")
+    try:
+        location = re.findall("<td class=\"th\">归属地</td>\n<td>\n<span>(.*?)</span>", content)[0].rstrip(" ")
+        if location.find(" ") != -1:
+            location = location.split(" ")[0]
+    except:
+        print(ip)
+        continue
+    translator = Translator()
+    translate = translator.translate(location, dest = "en").text
+    translate = translate[0].upper() + translate[1:]
+    table.write(ip + "\t" + translate + "\n")
+    name = translate + " - " + ip
+    os.mkdir(directory + "OpenVPN Config/" + name)
+    config_file = open(file, "r")
+    config = "".join(config_file.readlines()).replace("auth-user-pass", "auth-user-pass auth-user-pass.txt") + '''
+route douyu.com 255.255.255.255 net_gateway
+route zju.edu.cn 255.255.255.255 net_gateway
+route youdao.com 255.255.255.255 net_gateway
+route 10.109.92.4 255.255.255.255 net_gateway
+route 10.105.100.224 255.255.255.255 net_gateway
+route 10.109.92.5 255.255.255.255 net_gateway
+route 47.52.206.109 255.255.255.255 net_gateway
+route hoffman2.idre.ucla.edu 255.255.255.255 net_gateway
+'''
+    config_file.close()
+    config_file = open(directory + "OpenVPN Config/" + name + "/" + name + ".ovpn", "w")
+    config_file.write(config)
+    config_file.close()
+    shutil.copy(directory + "auth-user-pass.txt", directory + "OpenVPN Config/" + name + "/")
 table.close()
